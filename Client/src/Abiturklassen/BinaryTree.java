3a879d8c5bb78f787e164e64ab651ca1f99cb400
Mikerman <andrej_mikerman@web.de>
2016-01-20 19:39:43 +0100

    IsNumber versuch1
    Kopfweh spater

---
+++ Client/src/Abiturklassen/BinaryTree.java
@@ -0,0 +1,177 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Abiturklassen;
+
+public class BinaryTree
+{
+    private Object lContent;
+    private BinaryTree lLeftTree, lRightTree;
+
+    /**
+     * Nach dem Aufruf des Konstruktors existiert ein leerer Binaerbaum.
+     */
+    public BinaryTree()
+    {
+        lContent = null;
+        lLeftTree = null;
+        lRightTree = null;
+    }
+
+    /**
+     * Wenn der Parameter pContent ungleich null ist, existiert nach dem Aufruf des Konstruktors der Binaerbaum und hat pContent als Inhaltsobjekt
+     * und zwei leere Teilbaeume. Falls der Parameter null ist, wird ein leerer Binaerbaum erzeugt.
+     *
+     * @param pContent Inhaltsobjekt
+     */
+    public BinaryTree(Object pObject)
+    {
+        if(pObject != null)
+        {
+            lContent = pObject;
+            lLeftTree = new BinaryTree();
+            lRightTree = new BinaryTree();
+        } else
+        {
+            lContent = null;
+            lLeftTree = null;
+            lRightTree = null;
+        }
+    }
+
+    /**
+     * Wenn der Parameter pContent ungleich null ist, wird ein Binaerbaum mit pContent als Objekt
+     * und den beiden Teilbaeume pLeftTree und pRightTree erzeugt. Sind pLeftTree oder pRightTree gleich null, wird der entsprechende Teilbaum
+     * als leerer Binaerbaum eingefuegt. Wenn der Parameter pContent gleich null ist, wird ein leerer Binaerbaum erzeugt.
+     *
+     * @param pContent     Inhaltsobjekt
+     * @param pLinkerBaum  linker Binaerbaum
+     * @param pRechterBaum rechter Binaerbaum
+     */
+    public BinaryTree(Object pContent, BinaryTree pLinkerBaum, BinaryTree pRechterBaum)
+    {
+        if(pContent != null)
+        {
+            lContent = pContent;
+            if(pLinkerBaum != null)
+                lLeftTree = pLinkerBaum;
+            else
+                lLeftTree = new BinaryTree();
+            if(pRechterBaum != null)
+                lRightTree = pRechterBaum;
+            else
+                lRightTree = new BinaryTree();
+        } else
+        {    // da der Inhalt null ist, wird ein leerer Baum erzeugt
+            lContent = null;
+            lLeftTree = null;
+            lRightTree = null;
+        }
+
+    }
+
+    /**
+     * Diese Anfrage liefert den Wahrheitswert true, wenn der Binaerbaum leer ist, sonst liefert sie den Wert false.
+     *
+     * @return true, wenn der Binaerbaum leer ist, sonst false
+     */
+    public boolean isEmpty()
+    {
+        return (lContent == null);
+    }
+
+    /**
+     * Wenn der Binaerbaum leer ist, wird der Parameter pObject als Inhaltsobjekt sowie ein leerer linker und rechter Teilbaum eingefuegt.
+     * Ist der Binaerbaum nicht leer, wird das Inhaltsobjekt durch pObject ersetzt. Die Teilbaeume werden nicht geaendert.
+     * Wenn pObject null ist, bleibt der Binaerbaum unveraendert.
+     *
+     * @param pObject neues Inhaltsobjekt
+     */
+    public void setObject(Object pObject)
+    {
+        if(pObject != null)
+        {
+            if(this.isEmpty())
+            {
+                lLeftTree = new BinaryTree();
+                lRightTree = new BinaryTree();
+            }
+            lContent = pObject;
+        }
+    }
+
+    /**
+     * Diese Anfrage liefert das Inhaltsobjekt des Binaerbaums. Wenn der Binaerbaum leer ist, wird null zurueckgegeben.
+     *
+     * @return Inhaltsobjekt (bzw. null, wenn der Baum leer ist)
+     */
+    public Object getObject()
+    {
+        return lContent;
+    }
+
+    /**
+     * Wenn der Binaerbaum leer ist, wird pTree nicht angehaengt.
+     * Andernfalls erhaelt der Binaerbaum den uebergebenen Baum als linken Teilbaum. Falls der Parameter null ist, aendert sich nichts.
+     *
+     * @param pTree neuer linker Teilbaum
+     */
+    public void setLeftTree(BinaryTree pTree)
+    {
+        if(!this.isEmpty() && pTree != null)
+            lLeftTree = pTree;
+    }
+
+    /**
+     * Wenn der Binaerbaum leer ist, wird pTree nicht angehaengt.
+     * Andernfalls erhaelt der Binaerbaum den uebergebenen Baum als rechten Teilbaum. Falls der Parameter null ist, aendert sich nichts.
+     *
+     * @param pTree neuer rechter Teilbaum
+     */
+    public void setRightTree(BinaryTree pTree)
+    {
+        if(!this.isEmpty() && pTree != null)
+            lRightTree = pTree;
+    }
+
+    /**
+     * Diese Anfrage liefert den linken Teilbaum des Binaerbaumes.
+     * Der Binaerbaum aendert sich nicht. Wenn der Binaerbaum leer ist, wird null zurueckgegeben.
+     *
+     * @return linker Teilbaum
+     */
+    public BinaryTree getLeftTree()
+    {
+        if(!this.isEmpty())
+            return lLeftTree;
+        else
+            return null;
+    }
+
+    /**
+     * Diese Anfrage liefert den rechten Teilbaum des Binaerbaumes.
+     * Der Binaerbaum aendert sich nicht. Wenn der Binaerbaum leer ist, wird null zurueckgegeben.
+     *
+     * @return rechter Teilbaum
+     */
+    public BinaryTree getRightTree()
+    {
+        if(!this.isEmpty())
+            return lRightTree;
+        else
+            return null;
+    }
+
+    /**
+     * Der Baum wird durch diesen Auftrag geleert.
+     * Die Anfrage isEmpty liefert danach true zurueck und auf die beiden urspruenglichen Teilbaeume kann nicht mehr zugegriffen werden.
+     */
+    public void setEmpty()
+    {
+        lContent = null;
+        lLeftTree = null;
+        lRightTree = null;
+    }
+}
--- Client/src/Abiturklassen/main/Control.java
+++ Client/src/Abiturklassen/main/Control.java
@@ -1,84 +1,82 @@
 package Abiturklassen.main;
 
 import Abiturklassen.List;
+import Abiturklassen.BinaryTree;
 
-public class Control
-{
+public class Control {
+    
+    BinaryTree zahlenBaum;
+    public Control() {
+        
+        zahlenBaum =  new BinaryTree();
+        
+    }
+    private void erstelleBaum(BinaryTree pBaum,boolean pBol,int pInt)
+    {      
+        
+    }
+    
 
-    private List Gleichungen;
-
-    public Control()
-    {
-        Gleichungen = new List();
+    public boolean isNumber(boolean[] pZahlcode) {
+        
+       return true;
     }
 
-    public void moveMatch(List pEquasion, int pZahlIndex1, int pZahlIndex2, int pIndex1, int pIndex2)
-    {
 
-        if(pIndex1 >= 0 && pIndex1 <= 9
-           && pIndex2 >= 0 && pIndex2 <= 9)
-        {
+    public void moveMatch(List pEquasion, int pZahlIndex1, int pZahlIndex2, int pIndex1, int pIndex2) {
+
+        if (pIndex1 >= 0 && pIndex1 <= 9
+                && pIndex2 >= 0 && pIndex2 <= 9) {
             pEquasion.toFirst();
-            for(int i = 0; i < pZahlIndex1; i++)
-            {
+            for (int i = 0; i < pZahlIndex1; i++) {
                 pEquasion.next();
             }
-            Zahl Zahl1 = (Zahl)pEquasion.getObject();
+            Zahl Zahl1 = (Zahl) pEquasion.getObject();
             pEquasion.toFirst();
-            for(int i = 0; i < pZahlIndex2; i++)
-            {
+            for (int i = 0; i < pZahlIndex2; i++) {
                 pEquasion.next();
             }
-            Zahl Zahl2 = (Zahl)pEquasion.getObject();
+            Zahl Zahl2 = (Zahl) pEquasion.getObject();
 
             boolean zahl1[] = Zahl1.getZahlCode();
 
-            if(zahl1[pIndex1])
-            {
+            if (zahl1[pIndex1]) {
                 boolean zahl2[] = Zahl2.getZahlCode();
-                if(!zahl2[pIndex2])
-                {
+                if (!zahl2[pIndex2]) {
                     zahl1[pIndex1] = false;
                     zahl2[pIndex2] = true;
-                    Zahl1.setZahlCode(zahl1);
-                    Zahl2.setZahlCode(zahl2);
+                    if (this.isNumber(zahl1) && isNumber(zahl2)) {
+                        Zahl1.setZahlCode(zahl1);
+                        Zahl2.setZahlCode(zahl2);
+                    }
+
                 }
             }
 
         }
     }
 
-    public String matchToEquation(List pEquation)
-    {
+    public String matchToEquation(List pEquation) {
         String ausgabe = null;
         pEquation.toFirst();
-        while(pEquation.hasAccess())
-        {
-            if(!(pEquation.getObject() instanceof Zahl))
-            {
-                ausgabe += (char)pEquation.getObject();
-            }
-            else
-            {
+        while (pEquation.hasAccess()) {
+            if (!(pEquation.getObject() instanceof Zahl)) {
+                ausgabe += (char) pEquation.getObject();
+            } else {
 
-                ausgabe += ((Zahl)pEquation.getObject()).getZahl();
+                ausgabe += ((Zahl) pEquation.getObject()).getZahl();
             }
             pEquation.next();
         }
         return ausgabe;
     }
 
-    public List equationToMatch(String pEquation)
-    {
+    public List equationToMatch(String pEquation) {
         List ausgabe = new List();
-        for(int i = 0; i < pEquation.length(); i++)
-        {
-            if(!(pEquation.charAt(i) == '+' || pEquation.charAt(i) == '-' || pEquation.charAt(i) == '='))
-            {
+        for (int i = 0; i < pEquation.length(); i++) {
+            if (!(pEquation.charAt(i) == '+' || pEquation.charAt(i) == '-' || pEquation.charAt(i) == '=')) {
                 ausgabe.append(new Zahl(Integer.parseInt(pEquation.charAt(i) + "")));
-            }
-            else
-            {
+            } else {
 
                 ausgabe.append(pEquation.charAt(i));
             }
