428a86d88d18ed572f98d7a9959c43b47f4a5ad0
Mikerman <andrej_mikerman@web.de>
2016-01-20 15:14:42 +0100

    client Unterricht

--- Client/src/Abiturklassen/main/Control.java
+++ Client/src/Abiturklassen/main/Control.java
@@ -2,34 +2,87 @@ package Abiturklassen.main;
 
 import Abiturklassen.List;
 
-public class Control {
+public class Control
+{
 
     private List Gleichungen;
 
-    public Control() {
+    public Control()
+    {
         Gleichungen = new List();
     }
 
-    public void moveMatch(Zahl pZahl1, Zahl pZahl2, int pIndex1, int pIndex2) { // Klasse Zeichen (+,-,=) erstellen. Matches in der liste bewegen
-        if (pIndex1 >= 0 && pIndex1 <= 9
-                && pIndex2 >= 0 && pIndex2 <= 9) {
+    public void moveMatch(List pEquasion, int pZahlIndex1, int pZahlIndex2, int pIndex1, int pIndex2)
+    {
 
-            boolean zahl1[] = pZahl1.getZahlCode();
+        if(pIndex1 >= 0 && pIndex1 <= 9
+           && pIndex2 >= 0 && pIndex2 <= 9)
+        {
+            pEquasion.toFirst();
+            for(int i = 0; i < pZahlIndex1; i++)
+            {
+                pEquasion.next();
+            }
+            Zahl Zahl1 = (Zahl)pEquasion.getObject();
+            pEquasion.toFirst();
+            for(int i = 0; i < pZahlIndex2; i++)
+            {
+                pEquasion.next();
+            }
+            Zahl Zahl2 = (Zahl)pEquasion.getObject();
 
-            if (zahl1[pIndex1]) {
-                boolean zahl2[] = pZahl2.getZahlCode();
-                if (!zahl2[pIndex2]) {
+            boolean zahl1[] = Zahl1.getZahlCode();
+
+            if(zahl1[pIndex1])
+            {
+                boolean zahl2[] = Zahl2.getZahlCode();
+                if(!zahl2[pIndex2])
+                {
                     zahl1[pIndex1] = false;
                     zahl2[pIndex2] = true;
-                    pZahl1.setZahlCode(zahl1);
-                    pZahl2.setZahlCode(zahl2);
+                    Zahl1.setZahlCode(zahl1);
+                    Zahl2.setZahlCode(zahl2);
                 }
             }
 
         }
     }
+
     public String matchToEquation(List pEquation)
     {
-        //bynary serach tree?
+        String ausgabe = null;
+        pEquation.toFirst();
+        while(pEquation.hasAccess())
+        {
+            if(!(pEquation.getObject() instanceof Zahl))
+            {
+                ausgabe += (char)pEquation.getObject();
+            }
+            else
+            {
+
+                ausgabe += ((Zahl)pEquation.getObject()).getZahl();
+            }
+            pEquation.next();
+        }
+        return ausgabe;
+    }
+
+    public List equationToMatch(String pEquation)
+    {
+        List ausgabe = new List();
+        for(int i = 0; i < pEquation.length(); i++)
+        {
+            if(!(pEquation.charAt(i) == '+' || pEquation.charAt(i) == '-' || pEquation.charAt(i) == '='))
+            {
+                ausgabe.append(new Zahl(Integer.parseInt(pEquation.charAt(i) + "")));
+            }
+            else
+            {
+
+                ausgabe.append(pEquation.charAt(i));
+            }
+        }
+        return ausgabe;
     }
 }
