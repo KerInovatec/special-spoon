a6e952cdff2ffe33d0ce474c74ab69154f957a7d
Andrej <andrej_mikerman@web.de>
2016-01-25 11:15:33 +0100

    sssss

--- MatchBox.Client/src/de/matchbox/client/forms/FrmMain.java
+++ MatchBox.Client/src/de/matchbox/client/forms/FrmMain.java
@@ -8,7 +8,6 @@ public class FrmMain extends javax.swing.JFrame
 
     private static final long serialVersionUID = 1L;
     private Control control;
-    private Streichholzpanel shp;
 
     public FrmMain()
     {
@@ -38,6 +37,7 @@ public class FrmMain extends javax.swing.JFrame
         jMatch2_5 = new javax.swing.JLabel();
         jMatch2_6 = new javax.swing.JLabel();
         jMatch2_7 = new javax.swing.JLabel();
+        jButton3 = new javax.swing.JButton();
         mnuMain = new javax.swing.JMenuBar();
         mnuFile = new javax.swing.JMenu();
         mnuQuit = new javax.swing.JMenuItem();
@@ -95,6 +95,15 @@ public class FrmMain extends javax.swing.JFrame
 
         jMatch2_7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/de/matchbox/client/Resources/Horizontal.png"))); // NOI18N
 
+        jButton3.setText("jButton3");
+        jButton3.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         mnuFile.setText("Datei");
 
         mnuQuit.setText("Beenden");
@@ -129,44 +138,49 @@ public class FrmMain extends javax.swing.JFrame
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(83, 83, 83)
-                                .addComponent(jMatch1_1))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(71, 71, 71)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(83, 83, 83)
+                                        .addComponent(jMatch1_1))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(71, 71, 71)
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                                .addGroup(layout.createSequentialGroup()
+                                                    .addComponent(jMatch1_5)
+                                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                    .addComponent(jMatch1_6))
+                                                .addGroup(layout.createSequentialGroup()
+                                                    .addComponent(jMatch1_2)
+                                                    .addGap(98, 98, 98)
+                                                    .addComponent(jMatch1_3)))
+                                            .addGroup(layout.createSequentialGroup()
+                                                .addGap(10, 10, 10)
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                    .addComponent(jMatch1_4)
+                                                    .addComponent(jMatch1_7))))))
+                                .addGap(37, 37, 37)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(12, 12, 12)
+                                        .addComponent(jMatch2_1))
                                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                                         .addGroup(layout.createSequentialGroup()
-                                            .addComponent(jMatch1_5)
+                                            .addComponent(jMatch2_5)
                                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                            .addComponent(jMatch1_6))
+                                            .addComponent(jMatch2_6))
                                         .addGroup(layout.createSequentialGroup()
-                                            .addComponent(jMatch1_2)
+                                            .addComponent(jMatch2_2)
                                             .addGap(98, 98, 98)
-                                            .addComponent(jMatch1_3)))
+                                            .addComponent(jMatch2_3)))
                                     .addGroup(layout.createSequentialGroup()
                                         .addGap(10, 10, 10)
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                            .addComponent(jMatch1_4)
-                                            .addComponent(jMatch1_7))))))
-                        .addGap(37, 37, 37)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jMatch2_4)
+                                            .addComponent(jMatch2_7)))))
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(12, 12, 12)
-                                .addComponent(jMatch2_1))
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(jMatch2_5)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jMatch2_6))
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(jMatch2_2)
-                                    .addGap(98, 98, 98)
-                                    .addComponent(jMatch2_3)))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(10, 10, 10)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jMatch2_4)
-                                    .addComponent(jMatch2_7))))
+                                .addGap(171, 171, 171)
+                                .addComponent(jButton3)))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -195,11 +209,7 @@ public class FrmMain extends javax.swing.JFrame
                                     .addComponent(jMatch1_5)
                                     .addComponent(jMatch1_6))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jMatch1_7)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                                    .addComponent(jButton1)
-                                    .addComponent(jButton2)))
+                                .addComponent(jMatch1_7))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                                     .addGroup(layout.createSequentialGroup()
@@ -214,8 +224,13 @@ public class FrmMain extends javax.swing.JFrame
                                     .addComponent(jMatch2_5)
                                     .addComponent(jMatch2_6))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jMatch2_7)
-                                .addGap(0, 0, Short.MAX_VALUE)))))
+                                .addComponent(jMatch2_7)))
+                        .addGap(64, 64, 64)
+                        .addComponent(jButton3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton1)
+                            .addComponent(jButton2))))
                 .addContainerGap())
         );
 
@@ -237,6 +252,18 @@ public class FrmMain extends javax.swing.JFrame
         jButton2ActionPerformed(null);
     }//GEN-LAST:event_mnuQuitActionPerformed
 
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_jButton3ActionPerformed
+    {//GEN-HEADEREND:event_jButton3ActionPerformed
+        if(jMatch1_1.isVisible())
+
+            jMatch1_1.setVisible(false);
+        else
+        {
+            jMatch1_1.setVisible(true);
+        }
+
+    }//GEN-LAST:event_jButton3ActionPerformed
+
     public static void main(String args[])
     {
         java.awt.EventQueue.invokeLater(new Runnable()
@@ -252,6 +279,7 @@ public class FrmMain extends javax.swing.JFrame
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JList jList1;
     private javax.swing.JLabel jMatch1_1;
     private javax.swing.JLabel jMatch1_2;
