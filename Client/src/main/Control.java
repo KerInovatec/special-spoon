527efbc35ac5660d5e04e6c4c40bc04a7efaa63d
InfiniteSoul <infinitesoul155@gmail.com>
2016-01-25 10:12:52 +0100

    Revert "asd"
    This reverts commit 22ac847bbd9cb2408e8f8d7d45fa20e9a8777718.

--- Client/src/main/Control.java
+++ Client/src/main/Control.java
@@ -3,13 +3,15 @@ package main;
 import java.util.ArrayList;
 import main.abi.List;
 
-public class Control {
+public class Control
+{
 
     MatchBoxClient client;
 
     private FrmMain main;
 
-    public Control(FrmMain main) {
+    public Control(FrmMain main)
+    {
 
         Zahl hZahl = new Zahl();
         this.main = main;
@@ -17,26 +19,34 @@ public class Control {
 
     }
 
-    public boolean isNumber(boolean[] pBol) {
+    public boolean isNumber(boolean[] pBol)
+    {
         boolean ausgabe = false;
         int match;
         Zahl pZahl = new Zahl();
         boolean hBol[];
-        for (int i = 0; i <= 9; i++) {
+        for(int i = 0; i <= 9; i++)
+        {
             match = 0;
-            if (ausgabe) {
+            if(ausgabe)
+            {
                 return ausgabe;
             }
             pZahl.setZahl(i);
             hBol = pZahl.getZahlCode();
-            for (int y = 0; y < 7; y++) {
-                if (hBol[y] == pBol[y]) {
+            for(int y = 0; y < 7; y++)
+            {
+                if(hBol[y] == pBol[y])
+                {
                     match = +1;
-                } else {
+                }
+                else
+                {
                     match = - 1;
                 }
             }
-            if (match == 7) {
+            if(match == 7)
+            {
                 ausgabe = true;
             }
         }
@@ -44,29 +54,36 @@ public class Control {
 
     }
 
-    public void moveMatch(List pEquasion, int pZahlIndex1, int pZahlIndex2, int pIndex1, int pIndex2) {
+    public void moveMatch(List pEquasion, int pZahlIndex1, int pZahlIndex2, int pIndex1, int pIndex2)
+    {
 
-        if (pIndex1 >= 0 && pIndex1 <= 9
-                && pIndex2 >= 0 && pIndex2 <= 9) {
+        if(pIndex1 >= 0 && pIndex1 <= 9
+           && pIndex2 >= 0 && pIndex2 <= 9)
+        {
             pEquasion.toFirst();
-            for (int i = 0; i < pZahlIndex1; i++) {
+            for(int i = 0; i < pZahlIndex1; i++)
+            {
                 pEquasion.next();
             }
-            Zahl Zahl1 = (Zahl) pEquasion.getObject();
+            Zahl Zahl1 = (Zahl)pEquasion.getObject();
             pEquasion.toFirst();
-            for (int i = 0; i < pZahlIndex2; i++) {
+            for(int i = 0; i < pZahlIndex2; i++)
+            {
                 pEquasion.next();
             }
-            Zahl Zahl2 = (Zahl) pEquasion.getObject();
+            Zahl Zahl2 = (Zahl)pEquasion.getObject();
 
             boolean zahl1[] = Zahl1.getZahlCode();
 
-            if (zahl1[pIndex1]) {
+            if(zahl1[pIndex1])
+            {
                 boolean zahl2[] = Zahl2.getZahlCode();
-                if (!zahl2[pIndex2]) {
+                if(!zahl2[pIndex2])
+                {
                     zahl1[pIndex1] = false;
                     zahl2[pIndex2] = true;
-                    if (this.isNumber(zahl1) && isNumber(zahl2)) {
+                    if(this.isNumber(zahl1) && isNumber(zahl2))
+                    {
                         Zahl1.setZahlCode(zahl1);
                         Zahl2.setZahlCode(zahl2);
                     }
@@ -77,27 +94,37 @@ public class Control {
         }
     }
 
-    public String matchToEquation(List pEquation) {
+    public String matchToEquation(List pEquation)
+    {
         String ausgabe = null;
         pEquation.toFirst();
-        while (pEquation.hasAccess()) {
-            if (!(pEquation.getObject() instanceof Zahl)) {
-                ausgabe += (char) pEquation.getObject();
-            } else {
+        while(pEquation.hasAccess())
+        {
+            if(!(pEquation.getObject() instanceof Zahl))
+            {
+                ausgabe += (char)pEquation.getObject();
+            }
+            else
+            {
 
-                ausgabe += ((Zahl) pEquation.getObject()).getZahl();
+                ausgabe += ((Zahl)pEquation.getObject()).getZahl();
             }
             pEquation.next();
         }
         return ausgabe;
     }
 
-    public List equationToMatch(String pEquation) {
+    public List equationToMatch(String pEquation)
+    {
         List ausgabe = new List();
-        for (int i = 0; i < pEquation.length(); i++) {
-            if (!(pEquation.charAt(i) == '+' || pEquation.charAt(i) == '-' || pEquation.charAt(i) == '=')) {
+        for(int i = 0; i < pEquation.length(); i++)
+        {
+            if(!(pEquation.charAt(i) == '+' || pEquation.charAt(i) == '-' || pEquation.charAt(i) == '='))
+            {
                 ausgabe.append(new Zahl(Integer.parseInt(pEquation.charAt(i) + "")));
-            } else {
+            }
+            else
+            {
 
                 ausgabe.append(pEquation.charAt(i));
             }
@@ -105,7 +132,8 @@ public class Control {
         return ausgabe;
     }
 
-    void setRooms(ArrayList rooms) {
+    void setRooms(ArrayList rooms)
+    {
         main.setRooms(rooms);
     }
 }
