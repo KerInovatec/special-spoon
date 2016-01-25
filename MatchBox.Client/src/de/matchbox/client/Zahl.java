640260d40f151edc4be90270de3b6207b82bba7f
Andrej <andrej_mikerman@web.de>
2016-01-25 10:26:34 +0100

    ja

--- MatchBox.Client/src/de/matchbox/client/Zahl.java
+++ MatchBox.Client/src/de/matchbox/client/Zahl.java
@@ -3,111 +3,139 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.matchbox.client;
+package main;
 
 /**
  *
  * @author Name
  */
-public class Zahl {
+public class Zahl
+{
 
     private boolean[] zahlCode;
-    private int zahl;
 
-    public Zahl(int zahl) {
+    public Zahl(int zahl)
+    {
         setZahl(zahl);
     }
+
     public Zahl()
     {
-        
+
     }
 
-    public void setZahlCode(boolean[] zahlCode) {
+    public void setZahlCode(boolean[] zahlCode)
+    {
         this.zahlCode = zahlCode;
     }
-    
 
-    public void setZahl(int pZahl) {
-        if (pZahl >= 0 && pZahl <= 9) {
-            switch (pZahl) {
-                case 0: {
-                    zahlCode = new boolean[]{true, true, true, false, true, true, true};
+    public void setZahl(int pZahl)
+    {
+        if(pZahl >= 0 && pZahl <= 9)
+        {
+            switch(pZahl)
+            {
+                case 0:
+                {
+                    zahlCode = new boolean[]
+                    {
+                        true, true, true, false, true, true, true
+                    };
                     break;
                 }
-                case 1: {
-                    zahlCode = new boolean[]{false, false, true, false, false, true, false};
+                case 1:
+                {
+                    zahlCode = new boolean[]
+                    {
+                        false, false, true, false, false, true, false
+                    };
                     break;
                 }
-                case 2: {
-                    zahlCode = new boolean[]{true, false, true, true, true, false, true};
+                case 2:
+                {
+                    zahlCode = new boolean[]
+                    {
+                        true, false, true, true, true, false, true
+                    };
                     break;
                 }
-                case 3: {
-                    zahlCode = new boolean[]{true, false, true, true, false, true, true};
+                case 3:
+                {
+                    zahlCode = new boolean[]
+                    {
+                        true, false, true, true, false, true, true
+                    };
                     break;
                 }
-                case 4: {
-                    zahlCode = new boolean[]{false, true, true, true, false, true, false};
+                case 4:
+                {
+                    zahlCode = new boolean[]
+                    {
+                        false, true, true, true, false, true, false
+                    };
                     break;
                 }
-                case 5: {
-                    zahlCode = new boolean[]{true, true, false, true, false, true, true};
+                case 5:
+                {
+                    zahlCode = new boolean[]
+                    {
+                        true, true, false, true, false, true, true
+                    };
                     break;
                 }
-                case 6: {
-                    zahlCode = new boolean[]{true, true, false, true, true, true, true};
+                case 6:
+                {
+                    zahlCode = new boolean[]
+                    {
+                        true, true, false, true, true, true, true
+                    };
                     break;
                 }
-                case 7: {
-                    zahlCode = new boolean[]{true, false, true, false, false, true, false};
+                case 7:
+                {
+                    zahlCode = new boolean[]
+                    {
+                        true, false, true, false, false, true, false
+                    };
                     break;
                 }
-                case 8: {
-                    zahlCode = new boolean[]{true, true, true, true, true, true, true};
+                case 8:
+                {
+                    zahlCode = new boolean[]
+                    {
+                        true, true, true, true, true, true, true
+                    };
                     break;
                 }
-                case 9: {
-                    zahlCode = new boolean[]{true, true, true, true, false, true, true};
+                case 9:
+                {
+                    zahlCode = new boolean[]
+                    {
+                        true, true, true, true, false, true, true
+                    };
                     break;
                 }
             }
         }
-        this.zahl = pZahl;
+
     }
 
-    public boolean[] getZahlCode() {
+    public boolean[] getZahlCode()
+    {
         return zahlCode;
     }
 
-    public int getZahl() {
-        return zahl;
+    public boolean isNumber()
+    {
+        return this.toInt() != -1;
     }
-    
-    public static boolean isNumber(boolean[] pBol) {
-        boolean ausgabe = false;
-        int match;
-        Zahl pZahl = new Zahl();
-        boolean hBol[];
-        for (int i = 0; i <= 9; i++) {
-            match = 0;
-            if (ausgabe) {
-                return ausgabe;
-            }
-            pZahl.setZahl(i);
-            hBol = pZahl.getZahlCode();
-            for (int y = 0; y < 7; y++) {
-                if (hBol[y] == pBol[y]) {
-                    match = +1;
-                } else {
-                    match = - 1;
-                }
-            }
-            if (match == 7) {
-                ausgabe = true;
-            }
-        }
-        return ausgabe;
 
+    public int toInt()
+    {
+        for(int i = 0; i < 10; i++)
+            if(zahlCode == new Zahl(i).getZahlCode())
+                return i;
+        return -1;
     }
-    
+
 }
